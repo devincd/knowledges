ee10344b6f9f5e30264b4606972e44399412f51c
devincd <505259926@qq.com>
2019-10-14 16:08:51 +0800

    fmt

--- golang/package/fmt/fmt.go
+++ golang/package/fmt/fmt.go
@@ -8,7 +8,25 @@ var (
 	name string
 )
 
+type Human struct {
+	name string
+	age  int
+}
+
 func main() {
 	name = "caodi"
-	fmt.Println(name)
+
+	human := Human{
+		name:"caodi",
+		age:1,
+	}
+	fmt.Println(name, human)
+	//fmt.Printf("human: %v\n", human)
+	//fmt.Printf("human: %+v\n", human)
+	//
+	//fmt.Printf("human: %#v\n", human)
+	//fmt.Printf("human: %T\n", human)
+	//
+	//fmt.Printf("文字百分号: %%\n")
+	//fmt.Println(human)
 }
