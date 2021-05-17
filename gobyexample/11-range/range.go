fa7ae3481f10be1f1c6f7d5163debe55bdc97324
devincd <505259926@qq.com>
2021-05-17 14:25:16 +0800

    add range gobyexample
    Signed-off-by: devincd <505259926@qq.com>

---
+++ gobyexample/11-range/range.go
@@ -0,0 +1,60 @@
+/*
+range iterates over elements in a variety of data
+structures. Let's see how to use range with some of the
+data structures we've already learned.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	// Here we use range to sum the numbers in a slice. Arrays
+	// work like this too.
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	// range on arrays and slices provides both the index and
+	// value for each entry. Above we didn't need the index, so we
+	// ignored it with the blank identifier _. Sometimes we
+	// actually want the indexes thought.
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	// range on map iterates over key/value pairs.
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	// range can also iterate over just the keys of a map.
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	// range on strings iterates over Unicode code points. The
+	// first value is the starting byte index of the rune and the
+	// second the rune itself.
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
+
+/*
+OUTPUT
+$ go run range.go
+sum: 9
+index: 1
+b -> banana
+a -> apple
+key: a
+key: b
+0 103
+1 111
+*/
