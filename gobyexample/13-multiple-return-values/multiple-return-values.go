496df3123a719e11036d358cc7f22be1ba0bce23
devincd <505259926@qq.com>
2021-05-19 09:30:54 +0800

    add 3
    Signed-off-by: devincd <505259926@qq.com>

---
+++ gobyexample/13-multiple-return-values/multiple-return-values.go
@@ -0,0 +1,38 @@
+/*
+Go has built-in support for multiple return values. This
+feature us used often in idiomatic Go, for example to return
+both result and error values from a function.
+
+Accepting a variable number of arguments is another nice
+feature of Go functions; we'll look at this next.
+*/
+package main
+
+import "fmt"
+
+// The (int, int) in this function signature shows that the
+// function returns 2 ints.
+func vals() (int, int) {
+	return 3, 7
+}
+
+func main() {
+	// Here we use 2 different returns values from the call
+	// with multiple assignment.
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
+
+	// If you only want a subset of the returned values, use the
+	// blank identifier _.
+	_, c := vals()
+	fmt.Println(c)
+}
+
+/*
+OUTPUT
+$ go run multiple-return-values.go
+3
+7
+7
+*/
