0658eb0cd972bd079f5d2eba8764036bef1ea030
devincd <505259926@qq.com>
2021-06-02 09:33:50 +0800

    add comment in error gobyexample
    Signed-off-by: devincd <505259926@qq.com>

--- gobyexample/21-errors/errors.go
+++ gobyexample/21-errors/errors.go
@@ -14,14 +14,23 @@ import (
 	"fmt"
 )
 
+// By convention, errors are the last return value and have
+// type error, a built-in interface
 func f1(arg int) (int, error) {
 	if arg == 42 {
+		// errors.New constructs a basic error value with the given
+		// error message.
 		return -1, errors.New("can't work with 42")
 	}
-
+	// A nil value in the error position indicates that there was
+	// no error.
 	return arg + 3, nil
 }
 
+// It's possible to use custom types as errors by
+// implementing the Error() method on them. Here's a
+// variant on the example above that uses a custom type to
+// explicitly represent an argument error.
 type argError struct {
 	arg  int
 	prob string
@@ -33,12 +42,17 @@ func (e *argError) Error() string {
 
 func f2(arg int) (int, error) {
 	if arg == 42 {
+		// In thia case we use &argError syntax to build a new struct,
+		// supplying values the two fields arg and prob.
 		return -1, &argError{arg, "can't work with it"}
 	}
 	return arg + 3, nil
 }
 
 func main() {
+	// The two loops below test out each of our error-returning
+	// functions. Note that the use of an inline error check on the
+	// if line is a common idiom in Go code.
 	for _, i := range []int{7, 42} {
 		if r, e := f1(i); e != nil {
 			fmt.Println("f1 failed:", e)
@@ -53,6 +67,10 @@ func main() {
 			fmt.Println("f2 worked:", r)
 		}
 	}
+
+	// If you want to programmatically use the data in a custom
+	// error, you'll need to get the errors as an instance of the
+	// custom error type via type assertion.
 	_, e := f2(42)
 	if ae, ok := e.(*argError); ok {
 		fmt.Println(ae.arg)
