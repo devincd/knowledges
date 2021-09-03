987948bd6aeab661705e7380918a2ddea661c2a2
devincd <505259926@qq.com>
2021-09-03 09:53:32 +0800

    add defer to gobyexample
    Signed-off-by: devincd <505259926@qq.com>

---
+++ gobyexample/43-defer/defer.go
@@ -0,0 +1,63 @@
+/*
+Defer is used to ensure that a function call is performed
+later in a program's execution, usually for purposes of
+cleanup. defer is often used where e.g. ensure and
+finally would be used in other languages.
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// suppose we wanted to create a file, write to it, and then
+	// close when we're done. Here's how we could do that with
+	// defer.
+
+	// Immediately after getting a file object with createFile,
+	// we defer the closing of that file with closeFile. This will
+	// be executed at the end of the enclosing function(main),
+	// after writeFile has finished.
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
+
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+// It's important to check for errors when closing a file, even
+// in a deferred function.
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+/*
+Running the program confirms that the file is closed after
+being written.
+
+OUTPUT
+$ go run defer.go
+creating
+writing
+closing
+*/
