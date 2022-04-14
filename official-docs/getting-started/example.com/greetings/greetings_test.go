d4f4a75d59444f5b3528e67c48ec551cd2f1353e
devincd <505259926@qq.com>
2022-04-14 14:34:25 +0800

    finish getting started golang
    Signed-off-by: devincd <505259926@qq.com>

---
+++ official-docs/getting-started/example.com/greetings/greetings_test.go
@@ -0,0 +1,24 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
