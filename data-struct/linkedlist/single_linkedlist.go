161632f7a4d4f074f915abfb7f9ec9b034c3e01a
devincd <505259926@qq.com>
2023-03-10 11:31:30 +0800

    add data-struct
    Signed-off-by: devincd <505259926@qq.com>

---
+++ data-struct/linkedlist/single_linkedlist.go
@@ -0,0 +1,10 @@
+/*
+Linked List in Golang
+
+Singly-Linked list is a simple kind of linked list that allows traversal in
+one direction, for example forward. Each node in linked list containers the data
+part and the pointer to the next node in the linked list
+
+https://golangbyexample.com/singly-linked-list-in-golang/
+*/
+package main
